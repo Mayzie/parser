3de172cc0c1df731b40ffd1e43dc29c1c5efcc0f
Daniel Collis <dancol.95@gmail.com>
2015-10-03 21:18:39 +1000

    Define the various types of errors and their string eqivalents.

--- Imp/Source/Check/Error.hs
+++ Imp/Source/Check/Error.hs
@@ -5,7 +5,12 @@ module Imp.Source.Check.Error where
 -- | Problems we might find in the input program.
 data Error
         = ErrorNoMain
-
+        | ErrorUndefinedFunc String
+        | ErrorDuplicateFunc String
+        | ErrorInvalidArgs   String Int
+        | ErrorUndefinedVar  String
+        | ErrorDuplicateVar  String
+	| ErrorSyntax
 
 -- | Pretty print an error.
 prettyError :: Error -> String
@@ -13,3 +18,16 @@ prettyError err
  = case err of
         ErrorNoMain     
          -> "No main function defined."
+        ErrorUndefinedFunc func
+         -> "function '" ++ func ++ "' undefined."
+        ErrorDuplicateFunc func
+         -> "function '" ++ func ++ "' redefined."
+        ErrorInvalidArgs func args
+         -> "function '" ++ func ++ "' expects " ++ (show args) ++ " argument(s)."
+        ErrorUndefinedVar var
+         -> "function '" ++ var ++ "' undefined."
+        ErrorDuplicateVar var
+         -> "function '" ++ var ++ "' redefined."
+        ErrorSyntax
+         -> "Syntax Error."
+        _ -> ""
